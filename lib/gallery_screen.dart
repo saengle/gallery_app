301be0d35dcccbefb42f7bcdb49704b0c73eb831
saengle <zohv817@gmail.com>
2023-01-30 15:55:51 +0900

    Add: GalleryScreen

---
+++ lib/gallery_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class GalleryScreen extends StatefulWidget {
+  const GalleryScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GalleryScreen> createState() => _GalleryScreenState();
+}
+
+class _GalleryScreenState extends State<GalleryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('전자액자'),
+      ),
+      body: Container(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'gallery_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const GalleryScreen(),
     );
   }
 }
