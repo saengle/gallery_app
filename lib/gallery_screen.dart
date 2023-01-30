caec361c8a020c6a619580085faabad7c96fc190
saengle <zohv817@gmail.com>
2023-01-30 16:16:17 +0900

    Add: image_picker
    시작시 사진 선택하기 추가

--- lib/gallery_screen.dart
+++ lib/gallery_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class GalleryScreen extends StatefulWidget {
   const GalleryScreen({Key? key}) : super(key: key);
@@ -8,13 +9,27 @@ class GalleryScreen extends StatefulWidget {
 }
 
 class _GalleryScreenState extends State<GalleryScreen> {
+  final ImagePicker _picker = ImagePicker();
+
+  List<XFile>? images;
+
+  @override
+  void initState() {
+    super.initState();
+    loadImages();
+  }
+
+  Future<void> loadImages() async {
+    images = await _picker.pickMultiImage();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('전자액자'),
       ),
-      body: Container(),
     );
   }
 }
